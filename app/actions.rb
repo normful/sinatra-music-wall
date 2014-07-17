e57a412980941fdeb70479d076ece030b15a61ea
Norman Sue <normful@gmail.com>
2014-07-17 12:10:58 -0700

    add actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,32 @@
 get '/' do
   erb :index
 end
+
+get '/songs' do
+  @songs = Song.all
+  erb :'songs/index'
+end
+
+get '/songs/new' do
+  @song = Song.new
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    author:  params[:author],
+    url: params[:url]
+  )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  @more_songs = Song.where.not(id: params[:id]).where(author: @song.author)
+  erb :'songs/show'
+end
