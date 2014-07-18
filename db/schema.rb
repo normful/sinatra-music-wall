7543585bf3f5be98fcd52827b1f3122b08e9f1b1
Norman Sue <normful@gmail.com>
2014-07-17 18:31:43 -0700

    add users migration and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717190638) do
+ActiveRecord::Schema.define(version: 20140718013027) do
 
   create_table "songs", force: true do |t|
     t.string   "title"
     t.string   "author"
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
